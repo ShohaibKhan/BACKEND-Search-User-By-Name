016142eaeca845545940edcbbd9e5b768f68e1c3
ShohaibKhan <19691A04H9@mits.ac.in>
2023-01-16 17:01:24 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT=8080;
+let profile_url = "https://api.github.com/users/"
+const token = process.env.TOKEN;
+
+
+const bodyparser = require("body-parser");
+
+
+const headers = {
+   "Authorization":`Token ${token}`
+}
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res)=>{
+   
+})
+app.post('/getProfile',async(req,res)=>{
+   let rep_name = req.body.name
+   let prof = profile_url+rep_name
+   let rep = prof+"/repos"
+     
+   fetch(prof,{"method":"GET","headers":headers}).then((result)=>result.json())
+   .then((data)=>{
+      res.send(data);
+   })
+})
+app.post('/getRepo',async(req,res)=>{
+
+   let name = req.body.name
+   let prof  = profile_url+name
+   let rep = prof+"/repos?per_page=200"
+   fetch(rep,{"method":"GET","headers":headers}).then((result)=>result.json())
+   .then((data)=>{
+      res.send(data);
+   })
+  
+})
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}...`))
+
